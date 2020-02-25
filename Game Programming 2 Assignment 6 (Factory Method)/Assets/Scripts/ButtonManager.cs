e767a987954ebbfaff1b25b0bade9b3455020208
Godofwar1300 <55067215+Godofwar1300@users.noreply.github.com>
2020-02-25 01:59:05 -0600

    Got basic scenes and imported the Standard Assets to be used in the game

---
+++ Game Programming 2 Assignment 6 (Factory Method)/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void LoadLevel(string loadName)
+    {
+        switch(loadName)
+        {
+            case "Main Menu Scene":
+                SceneManager.LoadScene(0);
+                break;
+            case "Game Scene":
+                SceneManager.LoadScene(1);
+                break;
+            case "Game Over Scene":
+                SceneManager.LoadScene(2);
+                break;
+            case "Instructions Scene":
+                SceneManager.LoadScene(3);
+                break;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
