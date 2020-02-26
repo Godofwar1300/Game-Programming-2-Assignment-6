aaa42fce376ccf0c35468b85af647fd38c239f28
Godofwar1300 <55067215+Godofwar1300@users.noreply.github.com>
2020-02-25 20:33:45 -0600

    Completed the dialogue system, for the most part.

---
+++ Game Programming 2 Assignment 6 (Factory Method)/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    //private GameObject myDialogueBox;
+    public GameObject dialogueCanvasPrefab;
+    public GameObject dialogueCanvas;
+    private Text dialogueText;
+    private string dialogueString;
+
+    public string[] sentences;
+    public int currentSentence;
+    public int sentenceLength;
+
+    public float textSpeed;
+
+    public bool canNextLine;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueCanvasPrefab = Resources.Load("Prefabs/DialogueCanvas", typeof(GameObject)) as GameObject;
+        //dialogueCanvas.SetActive(false);
+        dialogueCanvas = Instantiate(dialogueCanvasPrefab);
+        dialogueCanvas.SetActive(false);
+
+        dialogueText = dialogueCanvas.GetComponentInChildren<Text>();
+        dialogueString = "";
+
+        currentSentence = -1;
+        //textSpeed = 0.05f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("The index is: " + currentSentence);
+
+        Debug.Log("canNextLine is: " + canNextLine);
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            textSpeed = .005f;
+        }
+
+        if (currentSentence != -1)
+        {
+
+            dialogueText.text = dialogueString;
+
+            if (Input.GetKeyDown(KeyCode.G) && canNextLine)
+            {
+                NextLine();
+            }
+
+        }
+
+    }
+
+    public void NextLine()
+    {
+        if (currentSentence + 1 == sentences.Length)
+        {
+            currentSentence = -1;
+            dialogueCanvas.SetActive(false);
+        }
+        else
+        {
+            textSpeed = .1f;
+            dialogueString = "";
+            currentSentence++;
+            sentenceLength = sentences[currentSentence].Length;
+            StartCoroutine(Type());
+        }
+    }
+
+    IEnumerator Type()
+    {
+
+        foreach (char letters in sentences[currentSentence].ToCharArray())
+        {
+
+            yield return new WaitForSeconds(textSpeed);
+
+            if(currentSentence != -1)
+            {
+                if(dialogueString.Length != sentenceLength)
+                {
+                    dialogueString += letters;
+                    canNextLine = false;
+
+                    if(dialogueString.Length == sentenceLength)
+                    {
+                        canNextLine = true;
+                    }
+                }
+                
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            dialogueCanvas.SetActive(true);
+            currentSentence = -1;
+
+            NextLine();
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            dialogueCanvas.SetActive(false);
+            dialogueString = "";
+            dialogueText.text = dialogueString;
+            currentSentence = -1;
+        }
+    }
+
+}
